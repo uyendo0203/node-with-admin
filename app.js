30813ed6916060a32d3a4b8e06deb72af4ec3b03
UyenDo <uyendo.0203@gmail.com>
2025-03-15 23:07:54 +0700

    done

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ require('dotenv').config();
 
 // Kết nối MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
+  connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds
+  socketTimeoutMS: 30000,   // Increase socket timeout to 30 seconds
+  // useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("✅ Kết nối MongoDB thành công"))
 .catch(err => console.error("❌ Lỗi kết nối MongoDB:", err));
@@ -25,14 +27,6 @@ app.set('layout', 'layouts/main'); // Default layout file (optional)
 app.set('view engine', 'ejs');
 
 // ================================================== 
-
-
-// Dữ liệu mẫu cho người dùng
-let users = [
-  { id: 1, username: 'user1', email: 'user1@example.com' },
-  { id: 2, username: 'user2', email: 'user2@example.com' },
-];
-
 // Khai báo thư mục views (nếu cần)
 app.set('views', path.join(__dirname, 'views'));
 
